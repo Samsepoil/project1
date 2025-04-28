11c1c7d8dc5446c00e023e12243ce9704f3285cb
ethanl5 <ethanlong5@gmail.com>
2025-04-28 12:00:11 -0400

    new file:   public/database.js

---
+++ public/database.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+
+var getID = require('mongodb').ObjectID();
+
+const url = 'mongodb://localhost:27017';
+
+const client = new MongoClient(url);
+
+let mongoClient = MongoClient(url,{ useUnifiedTopology: true });
+
+let myDB;
+
+async function connect(argdb) {
+    try {
+        await mongoClient.connect();
+
+        myDB = mongoClient.db(argdb);
+
+        if (!myDB) {
+            throw new Error("DB Connection Failed to start!");
+        }
+        else {
+            console.log(`Connected to ${argdb}`);
+            return myDB;
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+module.exports = connectToDatabase;
